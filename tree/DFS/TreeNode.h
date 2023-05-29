22c708dfbca2feaada2b451bbb520247121c0e1c
ruetrash <bigtimerr@163.com>
2023-05-29 21:02:08 +0800

    2023.5.29 2101  创建一个通用的头文件，每次创建新的文件不用再重复造轮子了。

---
+++ tree/DFS/TreeNode.h
@@ -0,0 +1,22 @@
+//
+// Created by ruetrash on 2023/5/29.
+//
+
+#ifndef TEST_TREENODE_H
+#define TEST_TREENODE_H
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#endif //TEST_TREENODE_H
